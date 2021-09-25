15668526632073f103975c5e08b8051ea8c551e7
VMois <vmois@protonmail.com>
2021-09-25 03:15:00 +0200

    init, first version with bootloader and VGA support for x86_64

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World! My name is {}", "Bubos");
+
+    loop {}
+}
